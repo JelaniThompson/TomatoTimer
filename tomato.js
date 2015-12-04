fa329ab22f7f33924323c43073de69457e019334
Jelani Thompson <jelanithompson@hotmail.com>
2015-12-03 20:07:28 -0500

    Keeping track of work periods and length periods

--- tomato.js
+++ tomato.js
@@ -1,4 +1,45 @@
-//HEY, REMEMBER TO USE DATE FUNCTIONS IN JAVASCRIPT FOR CALCULATING TIME STUFFS
-//THEY WORK FOR MILLISECONDS, SECONDS, MINUTES, HOURS, ETC. DESPITE THE NAME
+//Get the time the timer is supposed to end and subtract it from the current time to calculate time remaining
+function timeCalculations(endTime) {
+  var timeRemaining = Date.parse(endTime) - Date.parse(new Date());
+  //Perform .floor() to get whole seconds after minute has been counted
+  //Divide timeRemaining by 1000 to get seconds and not milliseconds
+  var seconds = Math.floor((timeRemaining/1000) % 60);
+  var minutes = Math.floor((timeRemaining/1000/60) % 60);
+}
 
-var sessionLength = null;
+//Settings for increment and decrement
+var settings = {
+  workPeriod : 0,
+  breakPeriod : 0
+};
+
+$('.button').on('click', checkPeriod);
+
+//Increment/Decrement work periods and breaks
+function checkPeriod() {
+  if( $(this).hasClass("incrementWork") ) {
+    settings.workPeriod += 1;
+    console.log("Work Length: " + settings.workPeriod);
+  }
+
+  else if ( $(this).hasClass("decrementWork") ) {
+      settings.workPeriod -= 1;
+      console.log("Work Length: " + settings.workPeriod);
+  }
+
+  else if ( $(this).hasClass("incrementBreak") ) {
+    settings.breakPeriod += 1;
+    console.log("Break Length: " + settings.breakPeriod);
+  }
+
+  else if ( $(this).hasClass("decrementBreak") ) {
+    settings.breakPeriod -= 1;
+    console.log("Break Length: " + settings.breakPeriod);
+  }
+}
+
+//Display timer in DOM
+var DOM = document.getElementById("timeDisplay");
+DOM.innerHTML = "Woah, hey guys!";
+
+//Counting down
