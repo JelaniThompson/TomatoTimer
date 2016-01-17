cd08a58b57cb6e1f0d8cde9dc59e4bef14c9623d
Jelani Thompson <jelanithompson@hotmail.com>
2016-01-16 21:20:59 -0500

    More States

--- tomato.js
+++ tomato.js
@@ -1,12 +1,13 @@
 'use strict';
 
-//Visual representation of work/break times
+//Visual representation of work/break times and counter
 let $workDisplay = $("#workNum"),
     $breakDisplay = $("#breakNum"),
     $workMinus = $(".workMinus"),
     $workPlus = $(".workPlus"),
     $breakMinus = $(".breakMinus"),
-    $breakPlus = $(".breakPlus");
+    $breakPlus = $(".breakPlus"),
+    $counter = $("#counter");
 
 //Initialize date object to work with functions (seven 0's to remove default local time)
 let time = new Date(0, 0, 0, 0, 0, 0, 0);
@@ -23,8 +24,8 @@ let setTime = function(minutes, seconds) {
 setTime(25, 0);
 
 let getMinutes = time.getMinutes(setTime.minutes);
-
 $workDisplay.text(getMinutes);
+$counter.text(getMinutes);
 
 //Timer states
 let toggleTimer = function(newState) {
@@ -33,31 +34,43 @@ let toggleTimer = function(newState) {
         state = newState || (state == 'on' ? 'off' : 'on');
     }
     console.log(state);
-    checkState();
 }
 
 $("#counter").on('click', () => toggleTimer());
-
-//Check state
-let checkState = function() {
-    if (state == 'on') {
-        countdown();
-    }
-}
-
-//Start working on countdown
-let countdown = function() {
-	//console.log("soak console.logs in wood");
-}
-
-//Update time display every second
-let updateTimeDisplay = window.setInterval(countdown, 1000);
 
 $workMinus.on('click', function() {
     getMinutes -= 1;
     console.clear();
     $workDisplay.text(getMinutes);
+    $counter.text(getMinutes);
     console.log(getMinutes);
 });
 
+$workPlus.on('click', function() {
+    getMinutes += 1;
+    console.clear();
+    $workDisplay.text(getMinutes);
+    $counter.text(getMinutes);
+    console.log(getMinutes);
+});
+
+//Count down seconds and minutes
+let countdownSecond = function() {
+    if (state == 'on') {
+        //Seconds stuff
+    }
+}
+
+let countdownMinute = function() {
+    if (state == 'on') {
+        $counter.text(getMinutes -= 1);
+    }
+}
+
+//Update time display every minute/second
+window.setInterval(countdownMinute, 60000)
+window.setInterval(countdownSecond, 1000);
+
 console.log(state);
+
+//REMEMBER. BREAK TIMER ONLY ACTIVATES ONCE WORK TIMER IS FINISHED. THIS MAKES EVERYTHING MUCH EASIER
