1e0a69f842a009c0545618858791a197a7b4b4f5
Jelani Thompson <jelanithompson@hotmail.com>
2016-02-07 18:22:21 -0500

    Pretty much finished, just finish refresh bug and then ready to submit to FCC

--- tomato.js
+++ tomato.js
@@ -13,6 +13,7 @@ let $workDisplay = $("#workNum"),
 
 //Create a date object for the start 
 let date = new Date(0, 0, 0, 0, 0, 0, 0);
+let breakCounter = new Date(0, 0, 0, 0, 0, 0, 0);
 
 //Alarm audio
 let alarmSound = new Audio('http://www.oringz.com/oringz-uploads/b2_oringz-pack-nine-17.mp3');
@@ -21,9 +22,11 @@ let state = 'off';
 
 //Default value for minutes (25)
 date.setMinutes(25);
+breakCounter.setMinutes(5);
 
 //Prevent delayed loading in
 $workDisplay.text(date.getMinutes());
+$breakDisplay.text(breakCounter.getMinutes());
 $counter.text(date.getMinutes());
 
 //Return seconds as M:SS
@@ -32,13 +35,33 @@ function displayTime(minutes, seconds) {
         $counter.text(date.getMinutes() + ":" + date.getSeconds());
     }
 
-    //If a number needs a 0 prepended with an if statement (<10)
+    //If a number needs a 0 prepend with an if statement (<10)
     if (state == "on") {
         if (date.getSeconds() < 10) {
             $counter.text(date.getMinutes() + ":" + "0" + date.getSeconds());
         }
     }
+
+    //Break display for when timer is done
+    if (state == "done") {
+        breakCounter.setSeconds(breakCounter.getSeconds() - 1);
+        $counter.text(breakCounter.getMinutes() + ":" + breakCounter.getSeconds());
+    }
+
+    if (state == "done") {
+        if (breakCounter.getSeconds() < 10) {
+            $counter.text(breakCounter.getMinutes() + ":" + "0" + breakCounter.getSeconds());
+        }
+
+        if (breakCounter.getMinutes() == 0 && breakCounter.getSeconds() == 0) {
+            state = "on";
+            date.setMinutes(25);
+            alarmSound.play();
+        }
+    }
 }
+
+setInterval(displayTime, 1000);
 
 let countdown = setInterval(function() {
     if (state == 'on') {
@@ -47,14 +70,11 @@ let countdown = setInterval(function() {
 
     //Set state to done to trigger alarm sound to play
     if (date.getMinutes() == 0 && date.getSeconds() == 0) {
-        console.log("Test: setting state to done");
         state = "done";
         clearInterval(countdown);
         alarmSound.play();
     }
 }, 1000);
-
-setInterval(displayTime, 1000);
 
 //Timer states
 let toggleTimer = function(newState) {
@@ -64,7 +84,6 @@ let toggleTimer = function(newState) {
         state = newState || (state == 'on' ? 'off' : 'on');
     }
     console.log(state);
-    //$counter.text(state);
 }
 
 $counter.on('click', () => toggleTimer());
@@ -86,4 +105,18 @@ $workPlus.on('click', function() {
     }
 });
 
-console.log(state);
+$breakMinus.on('click', function() {
+    if (state == "done" || "off" && breakCounter.getMinutes() > 1) {
+        breakCounter.setSeconds(0);
+        breakCounter.setMinutes(breakCounter.getMinutes() - 1);
+        $breakDisplay.text(breakCounter.getMinutes());
+    }
+});
+
+$breakPlus.on('click', function() {
+    if (state == "done" || "off" && breakCounter.getMinutes() < 59) {
+        breakCounter.setSeconds(0);
+        breakCounter.setMinutes(breakCounter.getMinutes() + 1);
+        $breakDisplay.text(breakCounter.getMinutes());
+    }
+});
